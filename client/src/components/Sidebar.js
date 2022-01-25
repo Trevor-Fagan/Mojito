d67914a3a548b8bbe8d239a2b3776b93efa58d24
Trevor Fagan <trevorfagan77@gmail.com>
2022-01-24 20:14:14 -0500

    Sidebar styles updated and tailwind css installed

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Sidebar.css';
 
 const Sidebar = () => {
-  return (<div>
+  return (<div className='sidebar'>
     <div className='logo'>
       Mojito
     </div>
