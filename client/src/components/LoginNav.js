9bf550b7acd4ab0952c9cf411b9eff97d1a6681a
Trevor Fagan <trevorfagan77@gmail.com>
2022-01-27 13:23:41 -0500

    Fixed auth UI components, should be fully functioning

--- client/src/components/LoginNav.js
+++ client/src/components/LoginNav.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { UserOutlined, LogoutOutlined, UserAddOutlined } from '@ant-design/icons';
 import { useAuth0 } from '@auth0/auth0-react';
+import LogoutButton from '../components/LogoutButton';
+import LoginButton from '../components/LoginButton';
 
 const LoginNav = () => {
     const { user, isAuthenticated } = useAuth0();
@@ -9,12 +11,11 @@ const LoginNav = () => {
   return isAuthenticated && <div>
       <ul>
           <li><NavLink activeStyle={{color: '#FFF'}} to='/my_account'><UserOutlined /><span className='text_icon_padding'>My Account</span></NavLink></li>
-          <li><NavLink activeStyle={{color: '#FFF'}} to='/sign_out'><LogoutOutlined /><span className='text_icon_padding'>Sign Out</span></NavLink></li>
+          <li><LogoutOutlined /> <LogoutButton /></li>
         </ul>
   </div> || 
         <ul>
-            <li><NavLink activeStyle={{color: '#FFF'}} to='/sign_up'><UserOutlined /><span className='text_icon_padding'>Sign Up</span></NavLink></li>
-            <li><NavLink activeStyle={{color: '#FFF'}} to='/sign_in'><UserOutlined /><span className='text_icon_padding'>Sign In</span></NavLink></li>
+            <li><UserOutlined /> <LoginButton /></li>
         </ul>
   ;
 };
