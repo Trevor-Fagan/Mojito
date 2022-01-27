b2a6562882309be8acd32800236cbf86dd0852a3
Trevor Fagan <trevorfagan77@gmail.com>
2022-01-27 12:34:37 -0500

    User authentication added through auth0

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import '../styles/SignUp.css';
+import LoginButton from '../components/LoginButton';
+import LogoutButton from '../components/LogoutButton';
 
 const SignUp = () => {
+
   return <div>
       <div className='form_container'>
-        <form>
-            <h2>Sign Up</h2>
-
-            <h3>Full Name</h3>
-            <input className='text_input' type='text' placeholder='your name...'></input>
-
-            <h3>Username</h3>
-            <input className='text_input' type='text' placeholder='username...'></input>
-
-            <h3>Email</h3>
-            <input className='text_input' type='text' placeholder='email...'></input>
-
-            <h3>Password</h3>
-            <input className='text_input' type='password' placeholder='password...'></input>
-
-            <br />
-            <input className='submit_input' type='submit' value='Submit' />
-        </form>
+        <LoginButton />
+        <LogoutButton />
       </div>
   </div>;
 };
