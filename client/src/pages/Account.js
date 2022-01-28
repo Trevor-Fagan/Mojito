8eb2e1608cf39a409e2731d2e103d506e7e0f861
Trevor Fagan <trevorfagan77@gmail.com>
2022-01-28 09:22:39 -0500

    Updated account page styles

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -9,9 +9,11 @@ const Account = () => {
     <div>
       <h2>My Account</h2>
 
-      <img src={user.picture} />
-      <h3>Name: {user.given_name} {user.family_name}</h3>
-      <h3>Email: {user.email}</h3>
+      <div>
+        <img style={{border: '2px solid gray', margin: '10px', borderRadius: '100%'}} src={user.picture} />
+        <h3>Name: {user.given_name} {user.family_name}</h3>
+        <h3>Email: {user.email}</h3>
+      </div>
     </div>
     )
 }
