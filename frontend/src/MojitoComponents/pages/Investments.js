958c26a882dec3d2ddd9a33372b9345b59206caf
Trevor Fagan <trevorfagan77@gmail.com>
2022-03-02 23:03:35 -0500

    Working for Miguel :)

--- frontend/src/MojitoComponents/pages/Investments.js
+++ frontend/src/MojitoComponents/pages/Investments.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { LineChart, Line } from 'recharts';
+
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+
+const renderLineChart = (
+  <LineChart width={400} height={400} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+  </LineChart>
+);
 
 const Investments = () => {
-  return <div>Investments</div>;
+  return(
+  <div>
+    <h2>Investments</h2>
+
+    <p>This is the investments page</p>
+  </div>
+  );
 };
 
 export default Investments;
