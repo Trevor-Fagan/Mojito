3cee76788ca8f4c29042e9393d15b7ebb3e0dba8
Miguel Sarenas <migui0323@gmail.com>
2022-03-22 23:43:20 -0400

    created card for account information

--- frontend/src/MojitoComponents/pages/MyAccount.js
+++ frontend/src/MojitoComponents/pages/MyAccount.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Card } from "react-bootstrap";
 
 const MyAccount = () => {
   const { user } = useAuth0();
   // isAuthenticated  , such as const { user, userAuthenticated }
 
-  return(
+  return (
     <div className="main_content">
-      <h2>My Account</h2>
+      <Card style={{ width: "36rem"}}>
+        <Card.Body>
+          <Card.Title>Account Information</Card.Title>
+          <Card.Text>
+            First Name: {user.given_name}
+            <br></br>
+            Last Name: {user.family_name}
+            <br></br>
+            Email: {user.email}
+          </Card.Text>
+        </Card.Body>
+      </Card>
 
       <div>
-        <img style={{border: '2px solid gray', margin: '10px', borderRadius: '100%'}} alt="logo" src={user.picture} />
-        <h3>Name: {user.given_name} {user.family_name}</h3>
-        <h3>Email: {user.email}</h3>
+        <img
+          style={{
+            border: "2px solid gray",
+            margin: "10px",
+            borderRadius: "100%",
+          }}
+          alt="logo"
+          src={user.picture}
+        />
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default MyAccount;
