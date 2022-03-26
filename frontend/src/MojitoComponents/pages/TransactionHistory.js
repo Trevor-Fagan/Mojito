e2bd766da3a07a9d1f13163efbb23878040c6e83
Dori Palmer <drp20a@my.fsu.edu>
2022-03-26 19:35:05 -0400

    updated transaction history page

--- frontend/src/MojitoComponents/pages/TransactionHistory.js
+++ frontend/src/MojitoComponents/pages/TransactionHistory.js
@@ -9,14 +9,19 @@ class TransactionHistory extends React.Component {
       <div className="main_content">
         <h2 className="page_header">Transaction History</h2>
 
-        <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="entertainment" />
-        <Transaction title="TARGET 103 #523" price="5.73" date="May 27th" category="finance" />
-        <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="food" />
-        <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="finance" />
-        <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="entertainment" />
-        <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="food" />
-        <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="food" />
+        <div style={{color: "#565656", width: "50%"}}>
+          Category <span style={{marginLeft: "20px"}}>Type</span> <span style={{float: "right", marginRight: "36px"}}>Price</span>
+        </div>
 
+        <div style={{width: "50%", float: "left"}}>
+          <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="entertainment" />
+          <Transaction title="TARGET 103 #523" price="5.73" date="May 27th" category="finance" />
+          <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="food" />
+          <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="finance" />
+          <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="entertainment" />
+          <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="food" />
+          <Transaction title="AMZ Purchase #428092" price="5.73" date="May 23rd" category="food" />
+        </div>
       </div>
     );
   }
