fd903dcc76a7fc02a80bd3b82bc45376a0fd5222
ChristopherQue <89031124+ChristopherQue@users.noreply.github.com>
2022-03-26 15:15:09 -0400

    Created Income Page
    Added dummy entries for income, to be populated with real data.

--- frontend/src/MojitoComponents/pages/Income.js
+++ frontend/src/MojitoComponents/pages/Income.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import "../../App.css";
+import Transaction from '../Transaction';
 
 const Income = () => {
   return <div className="main_content">
-    <div className='page_header'>Income</div>
+  <div className = 'page_header'>Income</div>
 
-    
-
+  <div style={{color: "#565656"}}>
+    Category <span style={{marginLeft: "20px"}}>Type</span> <span style={{float: "right", marginRight: "40px"}}>Price</span>
+  </div>
+  
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
+  <Transaction title="4Rivers #24234 WORKDAY" price="631.12" date="March 26" category="finance" />
   </div>;
 };
 
