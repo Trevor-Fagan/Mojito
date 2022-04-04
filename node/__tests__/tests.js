aeba3c5e031069f9e0f9152c80b267f55615ecb5
Miguel Sarenas <migui0323@gmail.com>
2022-04-04 16:20:03 -0400

    added test for getting values from the database

--- node/__tests__/tests.js
+++ node/__tests__/tests.js
@@ -5,7 +5,21 @@ describe("Database tests", () => {
   it("should get an object from the database", async () => {
     const res = await request(server).get("/backend/budget");
     expect(res.status).toEqual(200);
+    console.log(res.body);
     // expect the response to be an object
     expect(typeof res.body).toEqual("object");
+  });
+
+  it("Get values from the database", async () => {
+    const res = await request(server).get("/backend/budget");
+    expect(res.status).toEqual(200);
+    // console.log(res.body);
+    expect(res.body.Id).toEqual("123456789");
+    expect(res.body.Housing).toEqual(800);
+    expect(res.body.Entertainment).toEqual(49);
+    expect(res.body.Vacation).toEqual(1200);
+    expect(res.body.Car).toEqual(500);
+    expect(res.body.Clothing).toEqual(100);
+    expect(res.body.Misc).toEqual(49);
   });
 });
